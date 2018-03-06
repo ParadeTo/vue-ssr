ebfb2f6b111a79f014d88affbae0cdefd0239721
youxingzhi <youxingzhi@qq.com>
2018-03-06 17:18:21 +0800

    客户端渲染

--- server.js
+++ server.js
@@ -8,16 +8,21 @@
  const path = require('path')
  const { createBundleRenderer } = require('vue-server-renderer')
  const bundle = require('./dist/vue-ssr-server-bundle.json')
+ const clientManifest = require('./dist/vue-ssr-client-manifest.json')
  const server = express()
 
  const renderer = createBundleRenderer(bundle, {
-   template: fs.readFileSync('./index.template.html', 'utf-8')
+   template: fs.readFileSync('./index.template.html', 'utf-8'),
+   clientManifest
  })
 
 // 这个方法返回一个仅仅用来解析json格式的中间件。这个中间件能接受任何body中任何Unicode编码的字符。支持自动的解析gzip和 zlib。
 server.use(bodyParser.json());
 // 这个方法也返回一个中间件，这个中间件用来解析body中的urlencoded字符，只支持utf-8的编码的字符。同样也支持自动的解析gzip和 zlib。
 server.use(bodyParser.urlencoded({ extended: false }));
+
+// 静态文件
+server.use(express.static(path.join(__dirname, '/dist'), { maxAge: 2592000 * 1000 }));
 
 // api
 let items = { 1: {title: "item1", content: "item1 content"}}
